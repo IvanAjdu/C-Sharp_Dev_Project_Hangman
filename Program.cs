6ca664bb7da85b255725c867785606b686d88be9
IvanAjdu <ivan.ajdu@gmail.com>
2023-05-01 23:53:54 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Jeu_Du_Pendu
+{
+    internal class Program
+    {
+
+        static void DevinerMot(string mot)
+        {
+            Console.WriteLine(mot);
+        }
+
+
+        static void Main(string[] args)
+        {
+            string mot = "ELEPHANT";
+
+            DevinerMot(mot);
+        }
+    }
+}
