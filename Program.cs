669382a40e68f89de9d52f9014d15a980d6ecf2f
IvanAjdu <ivan.ajdu@gmail.com>
2023-05-04 07:29:12 +0200

    Method AfficherMot()

--- Program.cs
+++ Program.cs
@@ -3,17 +3,36 @@
     internal class Program
     {
 
-        static void DevinerMot(string mot)
+        static void AfficherMot(string mot, List<char> lettres)
         {
-            Console.WriteLine(mot);
+            // _ _ _ _ _ _ _ _ 
+            for (int i = 0; i < mot.Length; i++)
+            {
+                char lettre = mot[i];
+                if (lettres.Contains(lettre))
+                {
+                    Console.Write(lettre + " ");
+                }
+                else { Console.Write("_ "); }
+            }
+            Console.WriteLine();
         }
+
+        /*static void DevinerMot(string mot)
+        {
+            /// _ _ _ _ _ _ _ _
+            /// E _ E _ _ _ _ _
+            AfficherMot(mot);
+            // DemanderLettre();
+        }*/
 
 
         static void Main(string[] args)
         {
             string mot = "ELEPHANT";
 
-            DevinerMot(mot);
+            //DevinerMot(mot);
+            AfficherMot(mot, new List<char> { 'E', 'L', 'A'});
         }
     }
 }
